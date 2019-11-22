d17f4d5919bf3428ac7d73c4e00a535b52526ad9
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-22 14:18:12 +0900

    Search for our widget using a Finder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,5 +6,8 @@ import 'package:flutter_widget_test/main.dart';
 void main() {
   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
     await tester.pumpWidget(MyWidget(title: 'T', message: 'M'));
+
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
   });
 }
